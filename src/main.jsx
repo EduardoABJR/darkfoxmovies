855cddace69c0a398aaa1947c00ff1ea4bba1ece
Eduardo Bonelli jr <eduardo.abjr@gmail.com>
2023-06-21 12:14:51 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import BestMovies from './Pages/BestMovies.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <Routes>
       <Route element={<App/>}>
         <Route path='/' element={<Home />}> </Route>
